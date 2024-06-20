10da3b1f0f15984c34c5f4d25f3e5654865cae7a
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-06-20 17:13:59 +0300

    Оптимизирован метод по получению информации от API ч1.

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,5 @@
 class MarvelService {
+  // базовые строкчи для запроса в API
   _apiBase = "https://gateway.marvel.com:443/v1/public/characters"
   _apiKey = "apikey=8dc87586c41220020ff212b5e2226108"
   
@@ -11,13 +12,26 @@ class MarvelService {
 
     return await res.json();
   };
-
+  // Метод получения всех персонажей
   getAllCharacters = () => {
     return this.getResource(`${this._apiBase}?limit=9&offset=210&${this._apiKey}`);
   }
-
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}/${id}?${this._apiKey}`)
+  
+  // Метод получения одного персонажа
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}/${id}?${this._apiKey}`);
+    return this._transformCharacter(res);
+  }
+  
+  // Метод трансформации данных которые мы получаем в API
+  _transformCharacter = (res) => {
+    return {
+      name: res.data.results[0].name,
+      description: res.data.results[0].description,
+      thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+      homePage: res.data.results[0].urls[0].url,
+      wiki: res.data.results[0].urls[1].url,
+    }
   }
 }
 
