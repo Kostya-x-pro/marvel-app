058ea9db7aeec6ba66f9370bf70d3c35268dd176
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-06-28 13:35:44 +0300

    Добавлена навигация по приложению с помощью библеотеки React Router DOM v 5.0 (проект разделен в папке pages)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,30 +1,14 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
-
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
+import { MainPage, ComicsPage } from "../pages";
+// BrowserRouter - (Router) - главный компонент которы оборачивает все компоненты для работы маршрутизации
+// Route - маршрут по которому мы направляем наши компоненты
+// Switch - оборачивает все "Route" для настройки путей (что бы указывать конретные совпадения)
 
-import decoration from '../../resources/img/vision.png';
-
-// переписываем проект на функциональные компоненты и хуки
+// Атрибуты:
+// path - путь к нашей странице "принимает строку" ("/" - корень сайта)
+// exact - говорит атрибуту "path" что только полное совпадение необходимо рендарить
 const App = () => {
-    const [selectedChar, setChar] = useState(null)
-
-    const onCharselected = (id) => setChar(id);
-    
-    // BrowserRouter - (Router) - главный компонент которы оборачивает все компоненты для работы маршрутизации
-    // Route - маршрут по которому мы направляем наши компоненты
-    // Switch - оборачивает все "Route" для настройки путей (что бы указывать конретные совпадения)
-
-    // Атрибуты:
-    // path - путь к нашей странице "принимает строку" ("/" - корень сайта)
-    // exact - говорит атрибуту "path" что только полное совпадение необходимо рендарить
-
     return (
         <Router>
             <div className="app">
@@ -33,26 +17,13 @@ const App = () => {
                <Switch>
 
                     <Route exact path="/">
-                        <ErrorBoundary>
-                            <RandomChar/>
-                        </ErrorBoundary>  
-                        <div className="char__content">
-                            <ErrorBoundary>
-                                <CharList onCharselected={onCharselected} />
-                            </ErrorBoundary>
-                            <ErrorBoundary>
-                                <CharInfo charId={selectedChar}/>
-                            </ErrorBoundary>
-                        </div>
-                        <img className="bg-decoration" src={decoration} alt="vision"/>
+                       <MainPage/>
                     </Route>
                     
                     <Route exact path="/comics">
-                        <AppBanner/>
-                        <ErrorBoundary>
-                            <ComicsList/>
-                        </ErrorBoundary>
+                        <ComicsPage/>
                     </Route>
+                    
                </Switch>
             </main>
         </div>
