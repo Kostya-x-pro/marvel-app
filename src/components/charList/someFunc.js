03d2ea3a2bd154e866c5c22834d3fa4922378fe6
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-06-30 13:40:01 +0300

    Добавленно несколько примеров ленивой загрузки динамических импортов

---
+++ src/components/charList/someFunc.js
@@ -0,0 +1,13 @@
+export function logger() {
+  console.log('Hello world');
+}
+
+export function seccondLogger() {
+  console.log('seccond');
+}
+
+function defaultLogger() {
+  console.log('deffault');
+}
+
+export default defaultLogger;
