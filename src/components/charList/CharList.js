b55631d6c2d1d0e0a0a12146b377777368f943d2
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-07-01 20:10:34 +0300

    Добавленна библеотека TransitionGroup и добавленна анимация при загрузке персонажей

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,7 @@
 import {useState, useEffect, useRef} from 'react';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import useMarvelService from '../../services/MarvelService';
@@ -55,26 +57,32 @@ const CharList = (props) => {
             }
             
             return (
-                <li 
-                    className="char__item"
-                    tabIndex={0}
-                    // Складваем каждый элемент в массив рефов
-                    ref={(el => itemRefs.current[i] = el)}
+                <CSSTransition
                     key={i} // временно поставлен индекс т.к лезут ошибки с item.id
-                    onClick={() => {
-                        props.onCharselected(item.id)
-                        // Вызов метода по установке фокуса и класса активности
-                        focusOnitem(i)
-                    }}>
-                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-                        <div className="char__name">{item.name}</div>
-                </li>
+                    timeout={500}
+                    classNames={"char__item"}>
+                    <li 
+                        className="char__item"
+                        tabIndex={0}
+                        // Складваем каждый элемент в массив рефов
+                        ref={(el => itemRefs.current[i] = el)}
+                        onClick={() => {
+                            props.onCharselected(item.id)
+                            // Вызов метода по установке фокуса и класса активности
+                            focusOnitem(i)
+                        }}>
+                            <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                            <div className="char__name">{item.name}</div>
+                    </li>
+                </CSSTransition>
             )
         });
 
         return (
             <ul className="char__grid">
-                {items}
+                <TransitionGroup component={null}>
+                    {items}
+                </TransitionGroup>
             </ul>
         )
     }
