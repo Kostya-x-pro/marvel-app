df8c24443a3af9cd5cf8c0e7b6e8e15a18a330cd
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-07-01 14:13:56 +0300

    Добавлен функционал с подробным описанием по работе с библеотекой ReactTransitionGroup (вкладка CSStransition) стили из CSS

--- src/ReactTransitionGroup/ReactTransitionGroup.js
+++ src/ReactTransitionGroup/ReactTransitionGroup.js
@@ -1,4 +1,4 @@
-import { Transition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 import {useState} from 'react';
 import { Container } from 'react-bootstrap';
 import './ReactTransitionGroup.css';
@@ -6,38 +6,24 @@ import './ReactTransitionGroup.css';
 const Modal = (props) => {
 
   const duration = 300;
-  // Объект со стилями =>
-  const defaultStyle = {
-    transition: `all ${duration}ms ease-in-out`,
-    opacity: 0,
-    visibility: 'hidden'
-  }
-  // Объект который будет говорить о стилях на переходных этапах =>
-  const transitionStyles = {
-    entering: { opacity: 1, visibility: 'visible' },
-    entered:  { opacity: 1, visibility: 'visible' },
-    exiting:  { opacity: 0, visibility: 'hidden' },
-    exited:  { opacity: 0, visibility: 'hidden' },
-  };
+
+
 
     return (
+      // в CSSTransition - стили идут из фалов стилей
       // Главный компонент для выполнения перехода (проп in дожен менять своё состояние поэтому его получаем из стейта)
       // unmountOnExit - этот пропс отвечает за то что элемент элемент не будет находиться в DOM дереве до тех пор пока мы его не вызовем с помощью клика на кнопку
       // onEnter - скрывает кнопку если окно открыто
       // onExited - показывает кнопку если окно открыто
-        <Transition 
+        <CSSTransition 
           timeout={duration} 
           in={props.show} 
-          unmountOnExit
           onEnter={() => props.setShowTrigger(false)}
-          onExited={() => props.setShowTrigger(true)} >
-        {/* state - это встроенный функционал который контролируется библеотекой это:
-        (entering, entered, exiting,exited) */}
-         {state => (
-            <div className="modal mt-5 d-block" style={{
-          ...defaultStyle,
-          ...transitionStyles[state]
-        }}>
+          onExited={() => props.setShowTrigger(true)}
+          classNames="modal"
+          mountOnEnter
+          unmountOnExit>
+            <div className="modal mt-5 d-block">
               <div className="modal-dialog">
                   <div className="modal-content">
                   <div className="modal-header">
@@ -54,8 +40,7 @@ const Modal = (props) => {
                   </div>
               </div>
             </div>
-         )}
-        </Transition>
+        </CSSTransition>
     )
 }
 
