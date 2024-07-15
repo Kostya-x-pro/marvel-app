915a3093d2357ec841cbe03863bfce2bfffcfd25
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-07-15 17:28:53 +0300

    Updates

--- src/components/pages/index.js
+++
@@ -1,9 +0,0 @@
-import MainPage from "./MainPage";
-import ComicsPage from "./ComicsPage";
-import SingleComicPage from "./SingleComicPage/SingleComicPage";
-
-export {
-  MainPage,
-  ComicsPage,
-  SingleComicPage,
-}
